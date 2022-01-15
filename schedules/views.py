60f49da022e782cc22bb4967bbea02e1762cddf6
Allen <allenlin0728@gmail.comikikigit condigexitexit()git config --global user.email allenlin0728@gmail.com>
2022-01-14 17:34:54 -0700

    html

--- schedules/views.py
+++ schedules/views.py
@@ -3,6 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from datetime import datetime
+from django.utils.timezone import make_aware
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    current_time = datetime.now()
+    context = {
+        "now": make_aware(current_time)
+    }
+    return render(request, "schedules/index.html", context)
