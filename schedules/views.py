ee4acb065ad2e0bac541174b583bac9260ca908c
Allen <allenlin0728@gmail.comikikigit condigexitexit()git config --global user.email allenlin0728@gmail.com>
2022-01-29 16:12:17 -0700

    changing website layout with tailwind components

--- schedules/views.py
+++ schedules/views.py
@@ -1,3 +1,4 @@
+from calendar import month_name
 from django.shortcuts import render
 
 # Create your views here.
@@ -14,4 +15,8 @@ def index(request):
     return render(request, "schedules/index.html", context)
 
 def about(request):
-    return HttpResponse("Hi")
+    return HttpResponse("this is not done go away")
+
+def calender(request, month):
+    current_month = datetime.now().month
+    return HttpResponse(f"the month is {current_month} this also isn't done pls go away")
