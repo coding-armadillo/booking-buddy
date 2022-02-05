55e9d520af665407928e167b318415c2fd63f9e7
Allen <allenlin0728@gmail.comikikigit condigexitexit()git config --global user.email allenlin0728@gmail.com>
2022-02-05 15:49:42 -0700

    black formats

--- booking_buddy/urls.py
+++ booking_buddy/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('customers/', include("customers.urls")),
-    path('schedules/', include("schedules.urls")),
+    path("admin/", admin.site.urls),
+    path("customers/", include("customers.urls")),
+    path("schedules/", include("schedules.urls")),
 ]
--- customers/models.py
+++ customers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.http import HttpResponse
-# Create your models here.
 
+# Create your models here.
 
 
 def index(request):
