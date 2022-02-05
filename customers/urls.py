4886df4bb93d7e0cfa3f0d7d93671da13a8756a3
Zeheng Li <imzehengl@gmail.com>
2022-02-05 16:03:25 -0700

    format code

--- customers/urls.py
+++ customers/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('detail/<customer_id>/', views.detail, name= 'detail'),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("detail/<customer_id>/", views.detail, name="detail"),
 ]
--- customers/views.py
+++ customers/views.py
@@ -5,14 +5,17 @@ from django.http import HttpResponse
 
 from datetime import datetime
 from django.utils.timezone import make_aware
+
+
 def index(request):
     current_time = datetime.now()
-    context = {
-        "now":make_aware(current_time)
-    }
-    return render(request, "customers/index.html",context)
+    context = {"now": make_aware(current_time)}
+    return render(request, "customers/index.html", context)
+
+
 def about(request):
-    return HttpResponse('Hello to the about page')
+    return HttpResponse("Hello to the about page")
+
 
 def detail(request, customer_id):
     return HttpResponse(f"hello, customer with id: {customer_id}")
