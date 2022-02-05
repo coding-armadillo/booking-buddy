d1b275a706cd23259a2728e1560cebb0fb33f5fe
Zeheng Li <imzehengl@gmail.com>
2022-02-05 15:50:05 -0700

    format codes

--- schedules/urls.py
+++ schedules/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name = "about"),
-    path('calender/<month>/', views.calender, name='calender')
-
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("calender/<month>/", views.calender, name="calender"),
 ]
--- schedules/views.py
+++ schedules/views.py
@@ -7,17 +7,19 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.utils.timezone import make_aware
 
+
 def index(request):
     current_time = datetime.now()
-    context = {
-        "now": make_aware(current_time),
-        "dates" : list(range(1,31))
-    }
+    context = {"now": make_aware(current_time), "dates": list(range(1, 31))}
     return render(request, "schedules/index.html", context)
+
 
 def about(request):
     return HttpResponse("this is not done go away")
 
+
 def calender(request, month):
     current_month = datetime.now().month
-    return HttpResponse(f"the month is {current_month} this also isn't done pls go away")
+    return HttpResponse(
+        f"the month is {current_month} this also isn't done pls go away"
+    )
