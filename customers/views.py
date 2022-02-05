e7f45905753ad8dd13d704386f74ef391bb19f11
Ken <kenlin7654@gmail.com>
2022-02-05 15:27:39 -0700

    Better page

--- customers/views.py
+++ customers/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+from datetime import datetime
+from django.utils.timezone import make_aware
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    current_time = datetime.now()
+    context = {
+        "now":make_aware(current_time)
+    }
+    return render(request, "customers/index.html",context)
+def about(request):
+    return HttpResponse('Hello to the about page')
+
+def detail(request, customer_id):
+    return HttpResponse(f"hello, customer with id: {customer_id}")
