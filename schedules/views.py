4baa55b0edb6c9bd361508458862d033f1a8796d
Allen <allenlin0728@gmail.com>
2022-06-25 16:39:58 -0600

    ok then

--- schedules/views.py
+++ schedules/views.py
@@ -153,4 +153,5 @@ def get_calendar(request, year, month, day=None):
         "color": color,
     }
     print(day)
+    print(schedules)
     return render(request, "schedules/index.html", context)
